c732cbd92ecc8af498c32b6268e37ed6c634b429
antoniodeveloper10 <antonio.developer10@gmail.com>
2025-10-16 16:14:25 -0300

    criado componetes

--- pages/camera.py
+++ pages/camera.py
@@ -1,25 +1,14 @@
 import streamlit as st
-from PIL import Image
-from pyzbar.pyzbar import decode
 
-st.title("Leitor de Código de Barras no Celular")
+st.title("Webcam")
 
-# Abre a câmera do celular
-foto = st.camera_input("Aponte a câmera para o código de barras ou QR code")
+# Captura uma imagem da câmera
+img_file = st.camera_input("Capture an image")
 
-if foto:
-    st.image(foto)  # mostra a foto na tela
+if img_file:
+    # Salva a imagem capturada
+    with open("streamlit_pic.png", "wb") as f:
+        f.write(img_file.getvalue())
 
-    # Converte para PIL
-    imagem = Image.open(foto)
-
-    # Decodifica códigos de barra
-    codigos = decode(imagem)
-
-    if codigos:
-        for codigo in codigos:
-            st.success(f"Código lido: {codigo.data.decode('utf-8')}")
-    else:
-        st.warning("Nenhum código de barras detectado.")
 
 
